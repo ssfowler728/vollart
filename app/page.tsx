d219a494be58cda4486906c222b545446b787d74
Stephanie Fowler <stephfsd623@gmail.com>
2024-08-02 20:31:57 -0600

    Introduced utility function to resolve static asset URLs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+function staticUrl(path:string):string {
+  return `${process.env.NEXT_PUBLIC_STATIC_PREFIX}${path}`;
+}
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
-      <img src="/volleyball-cover.png" className="max-w-screen-md " /> 
+      <img src={staticUrl('/volleyball-cover.png')} className="max-w-screen-md " /> 
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 const nextConfig = {
     output: 'export',
+
+    basePath: process.env.NODE_ENV === 'production' ? '/vollart' : '',
 };
 
 export default nextConfig;
