2d1728a55ae208dcc191e49b5c80953997e82a21
Stephanie Fowler <stephfsd623@gmail.com>
2024-08-02 19:59:22 -0600

    Created basic homepage with volleyball graphic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <h1>Hello Vollart World!</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <img src="/volleyball-cover.png" className="max-w-screen-md " /> 
     </main>
   );
 }
